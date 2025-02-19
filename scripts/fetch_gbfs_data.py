e520c3a7b40371731b96cd83fc80b9f4a0d459e5
Hunter Lybbert <hunterlybbert@gmail.com>
2025-02-19 10:38:35 -0800

    save checkpoint for script.

---
+++ scripts/fetch_gbfs_data.py
@@ -0,0 +1,26 @@
+"""This file is used to fetch new gtfs data regularly."""
+
+import json
+from datetime import datetime
+from gbfs.services import SystemDiscoveryService
+
+
+LIME_SEATTLE = 'lime_seattle'
+FEED_NAME = 'free_bike_status'
+
+
+def run():  
+    ds = SystemDiscoveryService()
+    client = ds.instantiate_client(LIME_SEATTLE)
+
+    time_of_system_query = datetime.now()
+    system_status = client.request_feed(FEED_NAME)
+
+    output_filename_formatted = f"raw_data/bikeshare_system_status_{time_of_system_query.strftime('%Y-%m-%d_%H-%M-%S')}.json"
+
+    with open(output_filename_formatted, "w") as output_file:
+        json.dump(system_status["data"]["bikes"], output_file)
+
+
+if __name__ == "__main__":
+    run()
