12842e05cae0e27c80fb123601d19a35d91b97d8
Hunter Lybbert <hunterlybbert@gmail.com>
2025-02-19 23:48:37 -0800

    Update scheduler shell script.

--- scripts/scheduler.sh
+++ scripts/scheduler.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 for h in {0..23}; do
-  pmset schedule wake "$(date -v+1d +%m/%d/%Y) $h:59:00"
-  pmset schedule wake "$(date -v+1d +%m/%d/%Y) $h:29:00"
+  pmset schedule wake "$(date -v+1d +%m/%d/%Y) $h:59:57"
+  pmset schedule wake "$(date -v+1d +%m/%d/%Y) $h:29:57"
 done
 
